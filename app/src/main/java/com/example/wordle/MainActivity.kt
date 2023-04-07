a7d0ecd04a207add2db4bb4c29fec4df7dcb542d
Parkduksung <duksung1234@naver.com>
2023-04-07 15:43:29 +0900

    단어 검색.

--- app/src/main/java/com/example/wordle/MainActivity.kt
+++ app/src/main/java/com/example/wordle/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.wordle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getWords()
+    }
+
+    private fun getWords() {
+
+        val inputStream = assets.open("wordle_words.txt")
+
+        val string = inputStream.readBytes().toString(Charsets.UTF_8).split("\n")
+
+        Log.d("결과", string.size.toString())
+
     }
 }
